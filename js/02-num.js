a34f09f0ec6aecb3b033c26b2723ad6568b35a29
rainverted <MY_NAME@example.com>
2021-05-04 20:08:28 +0300

    numbers

---
+++ js/02-num.js
@@ -0,0 +1,32 @@
+/*
+NUMBERS
+teigiami, nulis, neigiami
+teigiami, neigiami
+desimtainiai, sveikieji
+tikri skaiciai, begaybes, NaN
+
+
++0
+-0
+
+*/
+
+const teigiamas = 5;
+const neigiamas = 0;
+const nulis = 0;
+const nulisMinus = -0;
+
+console.log(nulis, nulisMinus);
+
+const desimtainis = 3.14415446;
+console.log(desimtainis);
+
+const begalybe = Infinity;
+const begalybeMinus = -Infinity;
+console.log(begalybe, begalybeMinus);
+
+const nesamone = NaN;
+console.log(nesamone0);
+
+const puseZodzio = 'labas' / 2;
+console.log(puseZodzio);
