27885bd979d5ccf2eeb2d66226da43b883e38bab
rainverted <MY_NAME@example.com>
2021-05-04 19:54:41 +0300

    initial comit

---
+++ js/01-vars.js
@@ -0,0 +1,29 @@
+const metai = 2021;
+console.log(metai);
+
+const vardas = 'vardenis';
+console.log(vardas);
+
+const arSninga = false;
+console.log('Ar sninga:', arSninga);
+
+const arTeigiama = true;
+console.log('Ar teigiama', arTeigiama);
+
+const pazymiai = [10, 2, 8, 4];
+console.log(pazymiai);
+
+const zodynas = ['labas', 'rytas', 'sakau', 'tau'];
+console.log(zodynas);
+
+const petras = {
+    vardas: 'Petras',
+    amzius: 99,
+    gyvenamojiVieta: 'Gatve g. 1-3, Miestas',
+    vaikai:[ {
+        vardas: 'Onute'
+        amzius: 67
+        }
+    ]
+};
+console.log('petras');
