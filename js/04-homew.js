03b3b2a9cfa86642b18b93a4c556b18655a02ee2
rainverted <MY_NAME@example.com>
2021-05-05 18:06:09 +0300

    Homework

---
+++ js/04-homew.js
@@ -0,0 +1,16 @@
+// 3 kintamieji su skaiciais
+const teigiamas = 21;
+const neigiamas = -21;
+
+
+console.log(teigiamas, neigiamas);
+
+const puseSkaiciaus = 12 / 2;
+console.log(puseSkaiciaus);
+
+// 3 kintamieji su tekstu
+const menuo1 = 'Rugsejis'
+const menuo2 = 'Spalis'
+const junginys = 'ir'
+console.log(menuo1 + ' ' + junginys + ' ' + menuo2);
+
