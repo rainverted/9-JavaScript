ffc37b985dc776064077bd2bb0f557f5c2cb03c0
rainverted <MY_NAME@example.com>
2021-05-06 21:03:41 +0300

    stoteles

--- js/09-ternary.js
+++ js/09-ternary.js
@@ -1,44 +1,141 @@
-/*
-TERNARY OPERATOR
+const akys = 'green';
 
-klausimas ? teigiama : neigiama
+switch (akys) {
+    case 'blue':
+        console.log('Melynos');
+        break;
 
-logika - taip arba ne (lyginant su if/else)
-*/
+    case 'green':
+        console.log('Zalios');
+        break;
 
+    case 'brown':
+        console.log('Rudos');
+        break;
 
+    default:
+        console.log('Neatpazinta spalva');
+        break;
+}
 
-const arLyja = false;
-
-const sketis = arLyja ? 'pasiimk' : 'nereikia';     // pasiimk /nereikia
-console.log(sketis);
-
-const s1 = 4 < 2 ? 'daugiau' : 'ne daugiau';
-console.log(s1);
-
-let s2 = '';
-if (4 < 2) {
-    s2 = 'daugiau';
+if (akys === 'blue') {
+    console.log('Melynos');
+} else if (akys === 'green') {
+    console.log('Zalios');
+} else if (akys === 'brown') {
+    console.log('Rudos');
 } else {
-    s2 = 'ne daugiau';
+    console.log('Neatpazinta spalva');
 }
-console.log(s2);
 
-let s3 = 'ne daugiau';
-if (4 < 2) {
-    s3 = 'daugiau';
+console.log('--------------------');
+
+const day = 2.5;
+
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('Darbo diena');
+        break;
+
+    case 6:
+    case 7:
+        console.log('Savaitgalis');
+        break;
+
+    default:
+        console.log('Neatpazinta savaites diena');
+        break;
 }
-console.log(s3);
 
-console.log('-----------------------');
+if (day === 1 || day === 2 || day === 3 || day === 4 || day === 5) {
+    console.log('Darbo diena');
+} else if (day === 6 || day === 7) {
+    console.log('Savaitgalis');
+} else {
+    console.log('Neatpazinta savaites diena');
+}
+
+if (day >= 0 && day < 6) {
+    console.log('Darbo diena');
+} else if (day >= 6 && day < 7) {
+    console.log('Savaitgalis');
+} else {
+    console.log('Neatpazinta savaites diena');
+}
+
+console.log('------------');
 
 /*
-Nestintas ternary
+Arbatos paruosimo procesas:
+0) paimti puodeli
+1) isideti arbatos
+2) isideti saldikli
+3) uzpilti vandens
+4) ismaisyti
+5) gerti
 */
 
-const color = 'blue';
-const light = true;
+const processStep = 0;
 
-const x1 = color === 'blue' ? light ? 'Sviesiai melyna' : 'Tamsiai melyna' : color === 'red' ? 'Raudona' : 'Nezinoma spalva';
+switch (processStep) {
+    case 0:
+        console.log('paimti puodeli');
 
-console.log(x1);
+    case 1:
+        console.log('isideti arbatos');
+
+    case 2:
+        console.log('isideti saldikli');
+
+    case 3:
+        console.log('uzpilti vandens');
+
+    case 4:
+        console.log('ismaisyti');
+
+    case 5:
+        console.log('gerti');
+        break;
+
+    default:
+        console.log('Neatpazintas procesas');
+        break;
+}
+
+console.log('------------------');
+
+const stotele = 'Rudens2';
+
+switch (stotele) {
+    case 'Geniu':
+        console.log('Geniu');
+    case 'Tremtiniu':
+        console.log('Tremtiniu');
+    case 'Rudens':
+        console.log('Rudens');
+    case 'K. Mindaugo':
+        console.log('K. Mindaugo');
+    case 'Z. Tiltas':
+        console.log('Z. Tiltas');
+        break;
+
+    case 'Lvovo':
+        console.log('Lvovo');
+    case 'Vasaros':
+        console.log('Vasaros');
+    case 'Rudens2':
+        console.log('Rudens2');
+    case 'Tremtiniu2':
+        console.log('Tremtiniu2');
+    case 'N.V.Ziedas':
+        console.log('N.V.Ziedas');
+        break;
+
+    default:
+        console.log('Neatpazinta stotele');
+        break;
+}
