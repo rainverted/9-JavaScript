729efe9e2e15c4bb7cad2f67f1eca5472f091d4c
rainverted <MY_NAME@example.com>
2021-05-06 21:02:06 +0300

    ternary nesting

---
+++ js/09-ternary.js
@@ -0,0 +1,44 @@
+/*
+TERNARY OPERATOR
+
+klausimas ? teigiama : neigiama
+
+logika - taip arba ne (lyginant su if/else)
+*/
+
+
+
+const arLyja = false;
+
+const sketis = arLyja ? 'pasiimk' : 'nereikia';     // pasiimk /nereikia
+console.log(sketis);
+
+const s1 = 4 < 2 ? 'daugiau' : 'ne daugiau';
+console.log(s1);
+
+let s2 = '';
+if (4 < 2) {
+    s2 = 'daugiau';
+} else {
+    s2 = 'ne daugiau';
+}
+console.log(s2);
+
+let s3 = 'ne daugiau';
+if (4 < 2) {
+    s3 = 'daugiau';
+}
+console.log(s3);
+
+console.log('-----------------------');
+
+/*
+Nestintas ternary
+*/
+
+const color = 'blue';
+const light = true;
+
+const x1 = color === 'blue' ? light ? 'Sviesiai melyna' : 'Tamsiai melyna' : color === 'red' ? 'Raudona' : 'Nezinoma spalva';
+
+console.log(x1);
