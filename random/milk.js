1ea06c3286001b8fb93a8c1c4373e45a38bc5906
rainverted <MY_NAME@example.com>
2021-05-11 18:06:51 +0300

    infinity

--- random/milk.js
+++ random/milk.js
@@ -1,47 +1,72 @@
 /*
-UZDUOTIS
-
+UZDUOTIS:
+- gaminam pieno kokteilius
 - gaunam uzsakymu kieki per diena (vienetais)
 - gaunam vienos porcijos dydi (litrais)
 - vienos karves duodamo pieno kiekis per diena (litrais)
 - reikia rasti, kiek reikia tureti karviu, norint patenkinti visus uzsakymus?
 */
 
-function milk(uzsakymai, porcija, dienoslitrai) {
-
-    if (typeof uzsakymai !== 'number'){
-        return 'ERROR: PIRMAS PARAMETRAS TURI BUTI SKAICIUS'
+function milk(uzsakymai, porcija, pienoIsKarves) {
+    // validation
+    if (typeof uzsakymai === 'undefined') {
+        return 'ERROR: neduotas pirmasis parametras';
+    }
+    if (typeof uzsakymai !== 'number') {
+        return 'ERROR: pirmas parametras turi buti skaicius';
+    }
+    if (uzsakymai < 0) {
+        return 'ERROR: pirmas parametras negali buti neigiamas';
+    }
+    if (uzsakymai % 1 !== 0) {
+        return 'ERROR: pirmas parametras turi buti sveikasis skaicius';
     }
 
-    if (typeof porcija !== 'number'){
-        return 'ERROR: antras PARAMETRAS TURI BUTI SKAICIUS'
+    if (typeof porcija === 'undefined') {
+        return 'ERROR: neduotas antrasis parametras';
+    }
+    if (typeof porcija !== 'number') {
+        return 'ERROR: antras parametras turi buti skaicius';
+    }
+    if (porcija < 0) {
+        return 'ERROR: antras parametras negali buti neigiamas';
     }
 
-    if (typeof dienoslitrai !== 'number'){
-        return 'ERROR: trecias PARAMETRAS TURI BUTI SKAICIUS'
+    if (typeof pienoIsKarves === 'undefined') {
+        return 'ERROR: neduotas treciasis parametras';
+    }
+    if (typeof pienoIsKarves !== 'number') {
+        return 'ERROR: trecias parametras turi buti skaicius';
+    }
+    if (pienoIsKarves <= 0) {
+        return 'ERROR: trecias parametras turi buti didesnis uz nuli';
     }
 
-    if (dienoslitrai <= 0) {
-        return 'ERROR: trecias parametras turi buti didesnis uz nuli'
-    }
+    // logic
+    const reikiaPieno = uzsakymai * porcija;
+    const reikiaKarviu = Math.ceil(reikiaPieno / pienoIsKarves);
 
-    if (uzsakymai < 0){
-        return 'ERROR: pirmas PARAMETRAS negali buti neigiamas'
-    }
-
-
-    const kiekPieno = uzsakymai * porcija;
-    const kiekKarviu = kiekPieno / dienoslitrai;
-
-    return Math.ceil (kiekKarviu);
-
+    // return result
+    return reikiaKarviu;
 }
-console.log(milk('labas', -0.5,true), '->', 6);
-console.log(milk(100, 'labas' ,true), '->', 6);
-console.log(milk(-100, 0.5 ,'labas'), '->', 6);
 
-console.log(milk(-100, 0.5, 9), '->', 1);
+console.log(milk('labas', 0.5, -9));
+console.log(milk(100, true, -9));
+console.log(milk(100, 0.5, 'pasideram'));
+console.log(milk(-100, 0.5, 9));
+console.log(milk(100, -0.5, 9));
+console.log(milk(100, 0.5, -9));
+console.log(milk(100, 0.5));
+console.log(milk(100));
+console.log(milk());
+console.log(milk(2.5, 0.5, 9));
 
 console.log(milk(10, 0.5, 9), '->', 1);
 console.log(milk(100, 0.5, 9), '->', 6);
 
+console.log(milk(NaN, 0.5, 9));
+console.log(milk(Infinity, 0.5, 9));
+console.log(milk(10, NaN, 9), '->', 1);
+console.log(milk(100, Infinity, 9), '->', 6);
+console.log(milk(10, 0.5, NaN), '->', 1);
+console.log(milk(100, 0.5, Infinity), '->', 6);
