95e99c36ecb3f89539dce532267bbf19e33ce503
rainverted <MY_NAME@example.com>
2021-05-11 18:58:27 +0300

    transform strings

---
+++ js/23-transformString.js
@@ -0,0 +1,25 @@
+const s1 = 'labas';
+const s2 = 'RYTAS';
+const s3 = 'KaS AnTrAs';
+
+const s1u = s1.toUpperCase();
+const s1l = s1.toLowerCase();
+
+const s2u = s2.toUpperCase();
+const s2l = s2.toLowerCase();
+
+const s3u = s3.toUpperCase();
+const s3l = s3.toLowerCase();
+
+
+console.log(s1);
+console.log(s1u);
+console.log(s1l);
+
+console.log(s2);
+console.log(s2u);
+console.log(s2l);
+
+console.log(s3);
+console.log(s3u);
+console.log(s3l);
